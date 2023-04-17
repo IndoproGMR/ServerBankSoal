0dd45c0c39533fe073d2c87ec48a91265c0ac4bb
IndoproGMR <indoprogamer007@gmail.com>
2023-04-17 22:37:25 +0700

    memperbaiki link dan menghapus file

--- app/Helpers/form_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-function display_error($validation, $inputform)
-{
-    if ($validation->hasError($inputform)) {
-        return $validation->getError($inputform);
-    } else {
-        return false;
-    }
-}
