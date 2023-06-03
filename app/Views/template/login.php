b8ece6a5447f25b0c3cb3025ea5946a822eb9077
IndoproGMR <indoprogamer007@gmail.com>
2023-06-03 12:59:22 +0700

    a

---
+++ app/Views/template/login.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+    <input autocomplete="on" type="email" class="form-control" name="login" placeholder="<?= lang('Auth.email') ?>">
+    <br>
+    <input autocomplete="on" type="text" class="form-control" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>">
+
+
+
+    <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.loginAction') ?></button>
+</form>
